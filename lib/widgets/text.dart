713a5bfb7e3761cce85723dc4d54427b4d0b85dd
Hope Kagame <hopekagame1@gmail.com>
2024-08-04 19:56:06 +0300

    Rewrite widget tree

--- lib/widgets/text.dart
+++ lib/widgets/text.dart
@@ -4,15 +4,17 @@ import 'package:url_launcher/url_launcher.dart';
 class SocialWidget extends StatelessWidget {
   const SocialWidget({
     required this.iconData,
-    required this.placeholderText,
     required this.link,
+    this.placeholderText,
     this.placeholderStyle,
     this.iconSize,
     this.iconColor,
     super.key,
   });
 
-  final String placeholderText;
+  /// Ignore to display icon only
+  final String? placeholderText;
+
   /// Begin with `https://...`
   final String link;
   final IconData iconData;
@@ -22,54 +24,49 @@ class SocialWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Row(
-        children: [
-          Container(
-            margin: const EdgeInsets.all(4),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: iconSize ?? 30,
-                  width: iconSize ?? 30,
-                  child: Icon(
-                    iconData,
-                    size: iconSize ?? 30,
-                    color: iconColor ?? Colors.grey,
-                  ),
-                ),
-                const SizedBox(width: 10),
-                InkWell(
-                  onTap: () async {
-                    final newUrl = link.split('https://').last;
-                    final host = newUrl.split('/').first;
-                    final path = newUrl.split('/').last == host
-                        ? ''
-                        : newUrl.split('/').last;
-                    final uri = Uri(
-                      host: host,
-                      path: path,
-                      scheme: 'https',
-                    );
-                    if (await canLaunchUrl(uri)) {
-                      await launchUrl(uri,
-                          mode: LaunchMode.externalApplication);
-                    }
-                  },
-                  child: Text(
-                    placeholderText,
-                    style: placeholderStyle ??
-                        const TextStyle(
-                          fontSize: 20,
-                          color: Colors.grey,
-                        ),
-                  ),
-                )
-              ],
+    return InkWell(
+      onTap: () async {
+        final newUrl = link.split('https://').last;
+        final host = newUrl.split('/').first;
+        final path =
+            newUrl.split('/').last == host ? '' : newUrl.split('/').last;
+        final uri = Uri(
+          host: host,
+          path: path,
+          scheme: 'https',
+        );
+        if (await canLaunchUrl(uri)) {
+          await launchUrl(uri, mode: LaunchMode.externalApplication);
+        }
+      },
+      child: Container(
+        margin: const EdgeInsets.all(4),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: iconSize ?? 30,
+              width: iconSize ?? 30,
+              child: Icon(
+                iconData,
+                size: iconSize ?? 30,
+                color: iconColor ?? Colors.grey,
+              ),
             ),
-          )
-        ],
+            if (placeholderText != null) ...[
+              const SizedBox(width: 10),
+              Text(
+                placeholderText ?? '',
+                style: placeholderStyle ??
+                    const TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey,
+                    ),
+              )
+            ]
+          ],
+        ),
       ),
     );
   }
